feeb5383507b793ffb4436c35e7794087ad736be
dogura <daiki.ogura@soliton.co.jp>
2016-05-01 20:43:35 +0900

    split env var in env.py

---
+++ env.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import (print_function, division, absolute_import, unicode_literals, )
+
+
+import os
+
+
+FB_VERIFY_TOKEN = os.environ['FB_VERIFY_TOKEN']
+PAGE_ACCESS_TOKEN = os.environ['PAGE_ACCESS_TOKEN']
